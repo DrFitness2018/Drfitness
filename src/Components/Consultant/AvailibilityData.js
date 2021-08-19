95b087a5c92c8f6f78024f12dc24ba4fb0880a84
ShahzaibQadir <77103950+ShahzaibQadir@users.noreply.github.com>
2021-08-20 01:53:06 +0500

    Availability

---
+++ src/Components/Consultant/AvailibilityData.js
@@ -0,0 +1,27 @@
+export const appointments = [
+  {
+    title: 'Mr Wahaj Shera',
+    startDate: new Date(2021,7,20,9,30),
+    endDate: new Date(2021,7,20,10,0),
+    id: 0,
+    location: 'Room 1',
+  }, {
+    title: 'Mr Tulaib ',
+    startDate: new Date(2021,7,20,10,30),
+    endDate: new Date(2021,7,20,11,30),
+    id: 2,
+    location: 'Room 1',
+  }, {
+    title: 'Mr Mehmam',
+    startDate: new Date(2021,7,21,9,30),
+    endDate: new Date(2021,7,21,10,0),
+    id: 2,
+    location: 'Room 1',
+  },{
+    title: 'Mr Shahzaib',
+    startDate: new Date(2021,7,21,11,0),
+    endDate: new Date(2021,7,21,12,30),
+    id: 2,
+    location: 'Room 1',
+  }
+];
